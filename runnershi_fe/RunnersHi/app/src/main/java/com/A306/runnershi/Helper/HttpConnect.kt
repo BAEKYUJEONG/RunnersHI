1cdb4179bdb154b2728c81ec4ce07e645ea3ef42
구본혁 <qhsgur0126@gmail.com>
2021-04-28 23:45:42 +0900

    [modify] URL을 이용한 GET 방식 클래스 함수 구현

--- runnershi_fe/RunnersHi/app/src/main/java/com/A306/runnershi/Helper/HttpConnect.kt
+++ runnershi_fe/RunnersHi/app/src/main/java/com/A306/runnershi/Helper/HttpConnect.kt
@@ -1,16 +1,24 @@
 package com.A306.runnershi.Helper
 
 import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.io.BufferedReader
+import java.io.InputStream
 import java.io.InputStreamReader
+import java.io.OutputStream
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.URLConnection
 import java.util.*
 import kotlin.concurrent.thread
 
 class HttpConnect {
     var urlAddress:String = ""
     var headers:Map<String, Any>? = null
+    var params:Map<String, Any>? = null
 
     constructor(){}
 
@@ -23,6 +31,12 @@ class HttpConnect {
         this.headers = headers
     }
 
+    constructor(urlAddress:String, headers:Map<String, Any>, params:Map<String, Any>){
+        this.urlAddress = urlAddress
+        this.headers = headers
+        this.params = params
+    }
+
     // 접속 할 주소
     val url = setServerAddress(urlAddress)
     // 연결 생성
@@ -32,10 +46,34 @@ class HttpConnect {
         conn.requestMethod = "POST"
     }
 
-    fun get():String{
+    fun get():String?{
         conn.requestMethod = "GET"
         // 읽어오기
-        val inputStream = InputStreamReader(conn.inputStream, "UTF-8")
+        var result:String? = ""
+        runBlocking {
+            result = async (Dispatchers.IO){
+                readStream(conn)
+            }.await()
+        }
+        return result
+    }
+
+    fun delete(){
+        conn.requestMethod = "DELETE"
+    }
+
+    fun put(){
+        conn.requestMethod = "PUT"
+    }
+
+    suspend fun readStream(stream:HttpURLConnection):String{
+        if (this.params != null){
+            val outStream = stream.outputStream
+            var receivedParams:Map<String, Any>? = this.params
+
+        }
+
+        val inputStream = InputStreamReader(stream.inputStream, "UTF-8")
         val bufferedRead = BufferedReader(inputStream)
         var stringResult:String? = null
         val buf = StringBuffer()
@@ -48,16 +86,8 @@ class HttpConnect {
 
         return buf.toString()
     }
-
-    fun delete(){
-        conn.requestMethod = "DELETE"
-    }
-
-    fun put(){
-        conn.requestMethod = "PUT"
-    }
 }
 
  private fun setServerAddress(urlAddress:String?):URL{
-     return URL("https://jsonplaceholder.typicode.com/todos")
+     return URL("https://jsonplaceholder.typicode.com/todos/1")
  }
