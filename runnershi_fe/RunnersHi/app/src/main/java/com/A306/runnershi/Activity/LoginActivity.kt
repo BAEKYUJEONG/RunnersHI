ba80c4ee17e9a540c11222c0e127128b9043af2f
구본혁 <qhsgur0126@gmail.com>
2021-04-29 18:29:10 +0900

    [modify] 로그인 화면 구성 완료

--- runnershi_fe/RunnersHi/app/src/main/java/com/A306/runnershi/Activity/LoginActivity.kt
+++ runnershi_fe/RunnersHi/app/src/main/java/com/A306/runnershi/Activity/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.A306.runnershi.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,11 +14,31 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        loginButton.setOnClickListener(View.OnClickListener {
-            Log.e("테스트", "제발 나와라")
-            var params = mapOf("postId" to 3)
-            var httpConnect = HttpConnect("/posts", params)
-            Log.e("테스트", httpConnect.post())
-        })
+        // 회원가입 버튼 클릭
+        // 회원가입 페이지로 이동
+        registerButton.setOnClickListener {
+            val registerIntent:Intent = Intent(this,RegisterActivity::class.java)
+            startActivity(registerIntent)
+        }
+
+        // 로그인 버튼 클릭
+        // 서버 통신으로 로그인
+        loginButton.setOnClickListener {
+            val userEmail = emailInput.text.toString()
+            val userPwd = pwdInput.text.toString()
+            val loginParams = mapOf("email" to userEmail, "pwd" to userPwd)
+            // 서버 통신
+            val loginConnect = HttpConnect("/login", loginParams)
+        }
+
+        // 카카오 로그인 버튼 클릭
+        kakaoLogin.setOnClickListener {
+
+        }
+
+        // 네이버 로그인 클릭
+        naverLogin.setOnClickListener {
+
+        }
     }
 }
---
+++ runnershi_fe/RunnersHi/app/src/main/java/com/A306/runnershi/Model/User.kt
@@ -0,0 +1,15 @@
+package com.A306.runnershi.Model
+
+class User {
+    var token:String = ""
+    var id:String = ""
+    var email:String = ""
+    var expiryDate:String = ""
+    var flag:Int = 0
+    var hasCustom = 0
+    var pwd = ""
+    var signUpPath:String = ""
+    var userName:String = ""
+
+//    constructor(userId:String, pwd:String, userName:String, email:String)
+}
