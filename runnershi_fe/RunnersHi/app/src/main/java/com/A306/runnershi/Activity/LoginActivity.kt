260de5b0b51b37c9fdae46ffd78b04d41fb2b91c
구본혁 <qhsgur0126@gmail.com>
2021-04-30 17:15:52 +0900

    [modify] 카카오 로그인 세팅 완료, 구현 중

--- runnershi_fe/RunnersHi/app/src/main/java/com/A306/runnershi/Activity/LoginActivity.kt
+++ runnershi_fe/RunnersHi/app/src/main/java/com/A306/runnershi/Activity/LoginActivity.kt
@@ -3,14 +3,20 @@ package com.A306.runnershi.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.A306.runnershi.Helper.HttpConnect
 import com.A306.runnershi.R
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // 카카오 초기화
+        KakaoSdk.init(this, "8bedaa715b7d7d885ebb5ebd5a3b5f84")
 
         // 회원가입 버튼 클릭
         // 회원가입 페이지로 이동
@@ -35,11 +41,8 @@ class LoginActivity : AppCompatActivity() {
 
         // 카카오 로그인 버튼 클릭
         kakaoLogin.setOnClickListener {
-            val registerIntent:Intent = Intent(this,RegisterActivity::class.java).apply {
-                putExtra("loginType", "Social")
-            }
-            startActivity(registerIntent)
-            overridePendingTransition(0,0)
+            var keyHash = Utility.getKeyHash(this)
+            Log.d("KEYHASH", keyHash)
         }
 
         // 네이버 로그인 클릭
