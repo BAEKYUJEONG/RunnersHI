673eef44c64f1a52b4d232a1a992894fd11ebc36
백유정 <qordbwjd96@gmail.com>
2021-05-13 18:08:41 +0900

    [feat] Running Detail 부분 수정 및 상세 페이지 추가

--- runnershi_fe/RunnersHi/app/src/main/java/com/A306/runnershi/Fragment/Profile/RunAdapter.kt
+++ runnershi_fe/RunnersHi/app/src/main/java/com/A306/runnershi/Fragment/Profile/RunAdapter.kt
@@ -56,8 +56,8 @@ class RunAdapter : RecyclerView.Adapter<RunAdapter.RunViewHolder>() {
                 timeInMillis = run.timestamp
             }
 
-            val dateFormat = SimpleDateFormat("dd.MM.yy",Locale.getDefault())
-            tvDate.text = dateFormat.format(calender.time)
+//            val dateFormat = SimpleDateFormat("dd.MM.yy",Locale.getDefault())
+//            tvDate.text = dateFormat.format(calender.time)
 
             val avgSpeed = "${run.avgSpeed}km/h"
             tvAvgSpeed.text = avgSpeed
