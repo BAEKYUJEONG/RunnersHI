f0c9599aec76b820041f46d2727d649965af2aa3
백유정 <qordbwjd96@gmail.com>
2021-05-17 23:18:39 +0900

    [feat] Home timeline history xml, adapter 작업

---
+++ runnershi_fe/RunnersHi/app/src/main/java/com/A306/runnershi/Fragment/Home/TimelineAdapter.kt
@@ -0,0 +1,51 @@
+package com.A306.runnershi.Fragment.Home
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.A306.runnershi.Model.Run
+import com.A306.runnershi.R
+
+class TimelineAdapter : RecyclerView.Adapter<TimelineAdapter.TimelineViewHolder>() {
+
+    inner class TimelineViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    val diffCallback = object : DiffUtil.ItemCallback<Run>() {
+        override fun areItemsTheSame(oldItem: Run, newItem: Run): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Run, newItem: Run): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+    }
+
+    val differ = AsyncListDiffer(this, diffCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimelineViewHolder {
+        return TimelineViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_timeline,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: TimelineViewHolder, position: Int) {
+        val run = differ.currentList[position]
+
+        holder.itemView.apply {
+
+        }
+    }
+
+
+}
