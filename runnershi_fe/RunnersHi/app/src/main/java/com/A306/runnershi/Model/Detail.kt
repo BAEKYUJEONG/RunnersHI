8996e5099526b1288efdc6e44c6cf87dee41fbd2
백유정 <qordbwjd96@gmail.com>
2021-05-18 16:47:01 +0900

    [feat] Detail Model 추가

---
+++ runnershi_fe/RunnersHi/app/src/main/java/com/A306/runnershi/Model/Detail.kt
@@ -0,0 +1,9 @@
+package com.A306.runnershi.Model
+
+data class Detail (
+    val userName: String? = null,
+    var timestamp: Long = 0L,
+    var distance: Int = 0,
+    var time: String? = null,
+    var title: String? = null
+)
